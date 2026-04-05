f7eb048705df9a37e84bc002c8e9ff707d76a781
jcastroj1-cj <jcastroj1@unicartagena.edu.co>
2026-04-05 14:26:49 -0500

    declarando un arreglo de 10 enteros

---
+++ arreglode10/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+int[] misNumeros = new int[10];
+
+
+misNumeros[0] = 100; 
+misNumeros[9] = 900; 
+
+Console.WriteLine("El primer número es: " + misNumeros[0]);
+
+// Esto evita que la consola se cierre sola inmediatamente
+Console.WriteLine("Presiona cualquier tecla para salir...");
+Console.ReadKey();
