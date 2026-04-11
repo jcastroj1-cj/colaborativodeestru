a93700b69a7acb80d46735553c44fd9c1d1b325a
jcastroj1-cj <jcastroj1@unicartagena.edu.co>
2026-04-11 12:31:45 -0500

    recorriendo con for clasico

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ import random
 arreglo = [random.randint(1, 100) for _ in range(10)]
 
 print("Arreglo:", arreglo)
+
+#blucle for clasico
+print("\n--- for clasico---")
+for i in range(len(arreglo)):
+    print(f"arreglo{[i]}) = {arreglo[i]}")
+    
