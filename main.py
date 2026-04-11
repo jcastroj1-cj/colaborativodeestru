8b816d80cbb4ab397cced0776260739fdd079456
jcastroj1-cj <jcastroj1@unicartagena.edu.co>
2026-04-11 12:36:13 -0500

    agregando el for each

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import random
 arreglo = [random.randint(1, 100) for _ in range(10)]
 
 print("Arreglo:", arreglo)
-
-#blucle for clasico
-print("\n--- for clasico---")
+# Bucle for clásico (con índice)
+print("\n--- For clásico ---")
 for i in range(len(arreglo)):
-    print(f"arreglo{[i]}) = {arreglo[i]}")
+    print(f"arreglo[{i}] = {arreglo[i]}")
     
+#blucle for each
+print("\n--- For-each ---")
+for elemento in arreglo:
+    print(elemento)
